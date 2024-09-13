93b653557881fcf5fc79e85e1c27094584ea539b
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2024-09-13 11:11:39 +0400

    Add files via upload

--- src/components/AddAgent.jsx
+++ src/components/AddAgent.jsx
@@ -1,16 +1,5 @@
-import { useNavigate } from "react-router-dom";
 import styles from "../components/AddAgent.module.css";
 
 export const AddAgent = () => {
-  const navigate = useNavigate();
-
-  const handleButtonClick = () => {
-    navigate("/agents");
-  };
-
-  return (
-    <button className={styles.button} onClick={handleButtonClick}>
-      + აგენტის დამატება
-    </button>
-  );
+  return <button className={styles.button}>+ აგენტის დამატება</button>;
 };
