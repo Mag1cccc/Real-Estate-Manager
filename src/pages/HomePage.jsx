84dd9275d1a5e7ab6cd0911c44ba2af3da22de94
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2024-09-13 10:39:26 +0400

    Add files via upload

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,53 @@
 import { Header } from "../components/Header";
+import { useState } from "react";
+import styles from "./HomePage.module.css";
+
+import { RegionSelector } from "../components/RegionSelector";
+import { PriceCategory } from "../components/PriceCategory";
+import { Area } from "../components/Area";
+import { BedroomCategory } from "../components/BedroomCategory";
+import { AddListing } from "../components/AddListing";
+import { AddAgent } from "../components/AddAgent";
 
 export const HomePage = () => {
+  const [selectedRegion, setSelectedRegion] = useState("");
+  const [priceRange, setPriceRange] = useState({ minPrice: "", maxPrice: "" });
+  const [areaRange, setAreaRange] = useState({ minArea: "", maxArea: "" });
+  const [selectedBedroomRange, setSelectedBedroomRange] = useState("");
+
+  const handleRegionSelect = (region) => {
+    setSelectedRegion(region);
+    console.log("Selected Region:", region, selectedRegion);
+  };
+
+  const handlePriceRangeSelect = (range) => {
+    setPriceRange(range);
+    console.log("Selected Price Range:", range, priceRange);
+  };
+
+  const handleAreaRangeSelect = (range) => {
+    setAreaRange(range);
+    console.log("Selected Area Range:", range, areaRange);
+  };
+
+  const handleBedroomSelect = (bedroomRange) => {
+    setSelectedBedroomRange(bedroomRange);
+    console.log("Selected Bedroom Count:", bedroomRange, selectedBedroomRange);
+  };
   return (
-    <>
+    <main className={styles.homepage}>
       <Header />
-    </>
+      <main className={styles.main}>
+        <RegionSelector onSelectRegion={handleRegionSelect} />
+        <PriceCategory onSelectPriceRange={handlePriceRangeSelect} />
+        <Area onSelectAreaRange={handleAreaRangeSelect} />
+        <BedroomCategory onSelectBedroomRange={handleBedroomSelect} />
+
+        <div className={styles.rightDiv}>
+          <AddListing />
+          <AddAgent />
+        </div>
+      </main>
+    </main>
   );
 };
