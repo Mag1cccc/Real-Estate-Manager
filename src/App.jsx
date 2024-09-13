747c3afe15da0c8c80674d07338d8a781c4a52e7
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2024-09-13 11:11:25 +0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
-import { AgentsPage } from "./pages/AgentsPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/agents" element={<AgentsPage />} />
       </Routes>
     </Router>
   );
