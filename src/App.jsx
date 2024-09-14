b5dbed2a8552a0eae3c42c664c6403b2c8e90e44
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2024-09-14 15:21:28 +0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
