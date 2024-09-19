5d1c1d4c444e98a03e5b9890411bce414b17ab59
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2024-09-19 19:05:52 +0400

    Add files via upload

--- src/pages/AddListingPage.jsx
+++ src/pages/AddListingPage.jsx
@@ -1,39 +1,179 @@
+import { useState, useEffect } from "react";
 import styles from "./AddListingPage.module.css";
 import { Header } from "../components/Header";
-import { useState } from "react";
 import ForSaleRent from "../components/ForSaleRent";
-import { Location } from "../components/Location";
-import { ApartmentDetails } from "../components/ApartmentDetails ";
+import { Price } from "../components/Price";
+import { PropertyArea } from "../components/PropertyArea";
+import { NumberOfBedrooms } from "../components/NumberOfBedrooms";
+import { Description } from "../components/Description";
 import { ImageUpload } from "../components/ImageUpload";
 import { AgentDropdown } from "../components/AgentDropdown";
 import { CancelButton } from "../components/CancelButton";
-import { SubmitButton } from "../components/SubmitButton";
 import { useNavigate } from "react-router-dom";
 import { SubmitListingButton } from "../components/SubmitListingButton";
+import { useAddListing } from "../hooks/useAddListing";
+import { useRegions } from "../hooks/useRegions";
+import { useCities } from "../hooks/useCities";
+import { RegionSelect } from "../components/RegionSelect";
+import { CitySelect } from "../components/CitySelect";
 
 export const AddListingPage = () => {
   const navigate = useNavigate();
+  const mutation = useAddListing();
 
   const [forSaleRent, setForSaleRent] = useState("");
   const [address, setAddress] = useState("");
   const [postalCode, setPostalCode] = useState("");
   const [image, setImage] = useState(null);
+  const [selectedAgent, setSelectedAgent] = useState("");
+  const [addressError, setAddressError] = useState("");
+  const [postalCodeError, setPostalCodeError] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
 
-  const handleForSaleRentChange = (event) => {
-    setForSaleRent(event.target.value);
+  const [price, setPrice] = useState("");
+  const [area, setArea] = useState("");
+  const [bedrooms, setBedrooms] = useState("");
+  const [description, setDescription] = useState("");
+
+  const {
+    data: regions,
+    isLoading: regionsLoading,
+    error: regionsError,
+  } = useRegions();
+
+  const {
+    data: cities,
+    isLoading: citiesLoading,
+    error: citiesError,
+  } = useCities(selectedRegion);
+
+  const validateAddress = (value) => {
+    if (value.length < 2) {
+      setAddressError("✓ მინიმუმ ორი სიმბოლო");
+      return false;
+    } else {
+      setAddressError("");
+      return true;
+    }
   };
 
-  const handleAgentSelect = (agent) => {
-    setSelectedAgent(agent);
-    setFormErrors((prevErrors) => ({ ...prevErrors, agent: false }));
+  const validatePostalCode = (value) => {
+    if (!/^\d{4,5}$/.test(value)) {
+      setPostalCodeError("✓ უნდა იყოს 4 ან 5 მხოლოდ რიცხვები");
+      return false;
+    } else {
+      setPostalCodeError("");
+      return true;
+    }
+  };
+
+  const handleAddressChange = (e) => {
+    const value = e.target.value;
+    setAddress(value);
+    validateAddress(value);
+  };
+
+  const handlePostalCodeChange = (e) => {
+    const value = e.target.value;
+    setPostalCode(value);
+    validatePostalCode(value);
+  };
+
+  const handleRegionChange = (event) => {
+    const regionId = event.target.value;
+    setSelectedRegion(regionId);
+    setSelectedCity("");
+  };
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
+
+  const handlePriceChange = (value) => {
+    setPrice(value);
+  };
+
+  const handleAreaChange = (value) => {
+    setArea(value);
+  };
+
+  const handleBedroomsChange = (value) => {
+    setBedrooms(value);
+  };
+
+  const handleDescriptionChange = (value) => {
+    setDescription(value);
+  };
+
+  useEffect(() => {
+    console.log("Selected Region ID:", selectedRegion);
+  }, [selectedRegion]);
+
+  if (regionsLoading || citiesLoading) return <div>Loading...</div>;
+  if (regionsError)
+    return <div>Error fetching regions: {regionsError.message}</div>;
+  if (citiesError)
+    return <div>Error fetching cities: {citiesError.message}</div>;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (
+      addressError ||
+      postalCodeError ||
+      !address ||
+      !postalCode ||
+      !selectedRegion ||
+      !selectedCity ||
+      !price ||
+      !area ||
+      !bedrooms ||
+      !description ||
+      !image ||
+      !selectedAgent ||
+      !forSaleRent
+    ) {
+      alert("გთხოვთ, შეასწოროთ შეცდომები გაგზავნამდე.");
+      return;
+    }
+
+    Number(price);
+    Number(area);
+    Number(bedrooms);
+    Number(selectedRegion);
+    Number(selectedCity);
+    Number(forSaleRent);
+
+    const formData = new FormData();
+    formData.append("address", address);
+    formData.append("zip_code", postalCode);
+    formData.append("price", price);
+    formData.append("area", area);
+    formData.append("bedrooms", bedrooms);
+    formData.append("description", description);
+    formData.append("region_id", selectedRegion);
+    formData.append("city_id", selectedCity);
+    formData.append("is_rental", forSaleRent === "forRent" ? 1 : 0);
+    formData.append("agent_id", selectedAgent.id);
+
+    if (image) {
+      formData.append("image", image);
+    }
+
+    mutation.mutate(formData, {
+      onSuccess: (formData) => {
+        console.log("Agent added successfully:", formData);
+        navigate("/");
+      },
+      onError: (error) => {
+        console.error("Error submitting form:", error);
+      },
+    });
   };
 
   const handleCancel = () => {
     navigate("/");
-  };
-
-  const handleSubmit = () => {
-    alert("!");
   };
 
   return (
@@ -41,16 +181,104 @@ export const AddListingPage = () => {
       <Header />
       <h1 className={styles.title}>ლისტინგის დამატება</h1>
       <form className={styles.form}>
-        <ForSaleRent value={forSaleRent} onChange={handleForSaleRentChange} />
-        <Location
-          address={address}
-          onAddressChange={setAddress}
-          postalCode={postalCode}
-          onPostalCodeChange={setPostalCode}
-        />
-        <ApartmentDetails />
+        <ForSaleRent value={forSaleRent} onChange={setForSaleRent} />
+        <div className={styles.container}>
+          <h4>მდებარეობა</h4>
+          <div className={styles.inputContainer}>
+            <div>
+              <label htmlFor="address" className={styles.label}>
+                მისამართი *
+              </label>
+              <input
+                type="text"
+                id="address"
+                value={address}
+                onChange={handleAddressChange}
+                className={`${styles.input} ${
+                  addressError ? styles.errorInput : ""
+                }`}
+                required
+                minLength={2}
+              />
+              <p
+                className={`${styles.helperText} ${
+                  addressError
+                    ? styles.errorText
+                    : address.length > 0
+                    ? styles.validText
+                    : ""
+                }`}
+              >
+                {addressError ||
+                  (address.length === 0
+                    ? "✓ მინიმუმ ორი სიმბოლო"
+                    : "✓ მინიმუმ ორი სიმბოლო")}
+              </p>
+            </div>
+            <div>
+              <label htmlFor="postalCode" className={styles.label}>
+                საფოსტო ინდექსი *
+              </label>
+              <input
+                type="text"
+                id="postalCode"
+                value={postalCode}
+                onChange={handlePostalCodeChange}
+                className={`${styles.input} ${
+                  postalCodeError ? styles.errorInput : ""
+                }`}
+                required
+                pattern="\d*"
+              />
+              <p
+                className={`${styles.helperText} ${
+                  postalCodeError
+                    ? styles.errorText
+                    : postalCode.length > 0
+                    ? styles.validText
+                    : ""
+                }`}
+              >
+                {postalCodeError ||
+                  (postalCode.length === 0
+                    ? "✓ უნდა იყოს 4 ან 5 ნიშნა მხოლოდ რიცხვები"
+                    : "✓ უნდა იყოს 4 ან 5 ნიშნა მხოლოდ რიცხვები")}
+              </p>
+            </div>
+          </div>
+          <div className={styles.inputContainer}>
+            <RegionSelect
+              regions={regions}
+              selectedRegion={selectedRegion}
+              onRegionChange={handleRegionChange}
+            />
+            {selectedRegion && (
+              <CitySelect
+                regionId={selectedRegion}
+                cities={cities}
+                selectedCity={selectedCity}
+                onCityChange={handleCityChange}
+              />
+            )}
+          </div>
+        </div>
+        <div className={styles.apartmentDetails}>
+          <h4>ბინის დეტალები</h4>
+          <div className={styles.containerTwo}>
+            <Price price={price} onPriceChange={handlePriceChange} />
+            <PropertyArea area={area} onAreaChange={handleAreaChange} />
+          </div>
+          <NumberOfBedrooms
+            bedrooms={bedrooms}
+            onBedroomsChange={handleBedroomsChange}
+          />
+          <Description
+            description={description}
+            onDescriptionChange={handleDescriptionChange}
+          />
+        </div>
         <ImageUpload onImageChange={setImage} />
-        <AgentDropdown onAgentSelect={handleAgentSelect} />
+        <AgentDropdown onAgentSelect={setSelectedAgent} />
         <div className={styles.buttonsContainer}>
           <CancelButton onClick={handleCancel} />
           <SubmitListingButton onClick={handleSubmit} />
