59459a36006d2240fcc4f39a4f094fa29eb1b22c
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2024-09-21 22:25:52 +0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AddListingPage } from "./pages/AddListingPage";
+import { ApartmentDetail } from "./components/ApartmentDetail";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/apartment/:id" element={<ApartmentDetail />} />
           <Route path="/add-listing" element={<AddListingPage />} />
         </Routes>
       </Router>
